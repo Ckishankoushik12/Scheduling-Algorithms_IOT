30a02f1996250c7efd28b8a7ea7143aea932d551
Ckishankoushik <73593808+Ckishankoushik12@users.noreply.github.com>
2023-09-24 00:41:45 +0530

    Q2.py

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -1,59 +1,73 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[8]:
 
 
-class Patient:
-    def _init_(self, name, arrival_time, treatment_time, urgency_level):
-        self.name = name
-        self.arrival_time = arrival_time
-        self.treatment_time = treatment_time
-        self.urgency_level = urgency_level
-
-def FCFS(patients):
-    return sorted(patients, key=lambda x: x.arrival_time)
-
-def SJF(patients):
-    return sorted(patients, key=lambda x: x.treatment_time)
-
-def PS(patients):
-    return sorted(patients, key=lambda x: -x.urgency_level)
-
-def RR(patients, q=10):
-    queue = []
-    time = 0
-    order = []
-
-    while patients or queue:
-        available = [p for p in patients if p.arrival_time <= time]
-        queue.extend(available)
-        for p in available:
-            patients.remove(p)
-
-        if queue:
-            current_patient = queue.pop(0)
-            if current_patient.treatment_time > q:
-                time += q
-                current_patient.treatment_time -= q
-                queue.append(current_patient)
-            else:
-                time += current_patient.treatment_time
-                order.append(current_patient)
-        else:
-            time += 1
-
-    return order
+import queue
 
 patients = [
-    Patient('P1', 0, 30, 3),
-    Patient('P2', 10, 20, 5),
-    Patient('P3', 15, 40, 2),
-    Patient('P4', 20, 15, 4)
+    {"name": "A", "arrival_time": 0, "treatment_time": 30, "urgency_level": 3},
+    {"name": "B", "arrival_time": 10, "treatment_time": 20, "urgency_level": 5},
+    {"name": "C", "arrival_time": 15, "treatment_time": 40, "urgency_level": 2},
+    {"name": "D", "arrival_time": 20, "treatment_time": 15, "urgency_level": 4},
 ]
 
-print("FCFS:", [p.name for p in FCFS(patients)])
-print("SJF:", [p.name for p in SJF(patients)])
-print("PS:", [p.name for p in PS(patients)])
-print("RR:", [p.name for p in RR(patients)])
+def print_schedule(schedule):
+    print("Patient Schedule:")
+    for i, patient in enumerate(schedule):
+        print(f"{i + 1}. Patient {patient['name']} [Urgency:- {patient['urgency_level']}]")
+
+# FCFS Scheduling
+def fcfs_scheduling(patients):
+    return sorted(patients, key=lambda x: x['arrival_time'])
+
+# SJF Scheduling
+def sjf_scheduling(patients):
+    return sorted(patients, key=lambda x: x['treatment_time'])
+
+# Priority Scheduling
+def ps_scheduling(patients):
+    return sorted(patients, key=lambda x: x['urgency_level'], reverse=True)  # Higher urgency is higher priority
+
+# Round Robin Scheduling
+def rr_scheduling(patients, time_quantum):
+    queue_rr = queue.Queue()
+    for patient in patients:
+        queue_rr.put(patient)
+
+    schedule = []
+    current_time = 0
+
+    while not queue_rr.empty():
+        patient = queue_rr.get()
+        if patient['treatment_time'] <= time_quantum:
+            schedule.append(patient)
+            current_time += patient['treatment_time']
+        else:
+            schedule.append({"name": patient['name'], "arrival_time": current_time, "treatment_time": time_quantum, "urgency_level": patient['urgency_level']})
+            current_time += time_quantum
+            patient['treatment_time'] -= time_quantum
+            queue_rr.put(patient)
+
+    return schedule
+
+if __name__ == "__main__":
+    print("FCFS Scheduling:")
+    fcfs_schedule = fcfs_scheduling(patients)
+    print_schedule(fcfs_schedule)
+
+    print("\nSJF Scheduling:")
+    sjf_schedule = sjf_scheduling(patients)
+    print_schedule(sjf_schedule)
+
+    print("\nPriority Scheduling:")
+    ps_schedule = ps_scheduling(patients)
+    print_schedule(ps_schedule)
+
+    print("\nRound Robin Scheduling:")
+    time_quantum = 10
+    rr_schedule = rr_scheduling(patients, time_quantum)
+    print_schedule(rr_schedule)
+
 
